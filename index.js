7b51e7690b936f4324f6567c1e4694f1777f5bf3
jasmine0615 <101412448+jasmine0615@users.noreply.github.com>
2023-10-05 17:22:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (!uri || !sc) {
 const cache = {};
 app.get("/", (req, res) => {
   try {
-    console.log("helloo. use URL/api/blog-stats to get the required display of data and URL/api/blog-search?query=programming U may replace programming with your choice of word. Thanks!! The url is the site of the  page, for eg : https://assign-subspace.onrender.com");
+    res.json("helloo. use URL/api/blog-stats to get the required display of data and URL/api/blog-search?query=programming U may replace programming with your choice of word. Thanks!! The url is the site of the  page, for eg : https://assign-subspace.onrender.com");
   } catch (error) {
     console.error("An error occurred:", error.message);
     res.status(500).json({
